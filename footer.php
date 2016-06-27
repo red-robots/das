1eb101d5a73947f697a389c56af38cd02d2476be
Fritz <fre.healy@gmail.com>
2016-06-27 15:44:08 -0400

    first draft of homepage html, and minor edits to footer

--- footer.php
+++ footer.php
@@ -17,24 +17,40 @@
 		<div class="wrapper">
             <div class="float-left">
                 <div class="company-info">
-                    <p><?php the_field("company_name","option");?></p>
-                    <p><?php the_field("main_line_telephone_number","option");?></p>
-                    <p>fax <?php the_field("fax_line_number","option");?></p>
-                    <p>toll free <?php the_field("toll_free_line_number","option");?></p>
+                    <?php if(get_field("company_name","option")): ?>
+                        <p><?php the_field("company_name","option");?></p>
+                    <?php endif; ?>
+                    <?php if(get_field("main_line_telephone_number","option")): ?>
+                        <p><?php the_field("main_line_telephone_number","option");?></p>
+                    <?php endif; ?>
+                    <?php if(get_field("fax_line_number","option")): ?>
+                        <p>fax <?php the_field("fax_line_number","option");?></p>
+                    <?php endif; ?>
+                    <?php if(get_field("toll_free_line_number","option")): ?>
+                        <p>toll free <?php the_field("toll_free_line_number","option");?></p>
+                    <?php endif; ?>
                 </div><!-- .company-info -->
                 <div class="footer-links-menu">
                     <?php wp_nav_menu( array( 'menu_id' => 'footer-menu' ) ); ?>
-                </div>
+                </div><!--.footer-links-menu-->
             </div> <!-- .float-left -->
             <div class="float-right">
                 <div class="company-location">
-                    <p><?php the_field("location","option");?></p>
-                    <div class="google-maps-location"><?php the_field("google_maps_location","option");?></div>
-                    <p><?php the_field("address_line_1","option");?> | <?php the_field("address_line_2","option");?></p>
-                    <p><?php the_field("city_state_zip","option");?></p>
+                    <?php if(get_field("location","option")): ?>
+                        <p><?php the_field("location","option");?></p>
+                    <?php endif;?>
+                    <?php if(get_field("google-maps-location","option")): ?>
+                        <div class="google-maps-location"><?php the_field("google_maps_location","option");?></div>
+                    <?php endif; ?>
+                    <?php if(get_field("address_line_1","option")&& get_field("address_line_2","option")): ?>
+                        <p><?php the_field("address_line_1","option");?> | <?php the_field("address_line_2","option");?></p>
+                    <?php endif;?>
+                    <?php if(get_field("city_state_zip","option")): ?>
+                        <p><?php the_field("city_state_zip","option");?></p>
+                    <?php endif;?>
                     <div class="sitemap-bw-menu">
                         <?php wp_nav_menu( array( 'menu_id' => 'sitemap-bw-menu' ) ); ?>
-                    </div>
+                    </div><!--.sitemap-bw-menu-->
                 </div><!-- .company-location -->
             </div><!-- .float-right -->
         </div><!-- wrapper -->
--- index.php
+++ index.php
@@ -20,37 +20,91 @@ get_header(); ?>
 		<?php
 		if ( have_posts() ) :
 
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-
-			<?php
-			endif;
-
 			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
+			while ( have_posts() ) : the_post(); ?>
+                <section class="homepage-section-slider">
+                    <div class="float-left">
+                        <?php get_sidebar("home"); ?>
+                    </div><!-- .float-left -->
+                    <div class="slides">
+                        <?php if(have_rows("slider_projects"):
+                            while(have_rows("slider_projects"): the_row();
+                                $image_url=wp_get_attachment_url(get_sub_field("image"));
+                                $project_title=the_sub_field("project_title");
+                                $project_type=the_sub_field("project_type");
+                                if($image_url && $project_title && $project_type)
+                                ?>
+                                    <div class="slide">
+                                        <img src="<?php echo $image_url;?>" alt="<?php echo $project_title?>" class="image">
+                                        <div class="info">
+                                            <h2 class="title"><?php echo $project_title; ?></h2>
+                                            <h3 class="type"><?php echo $project_type; ?></h3>
+                                        </div><!--.info-->
+                                    </div><!--.slide-->
+                                <?php endif; //if for the slide ?>
+                            <?php endwhile; // while for slides as a whole ?>
+                        <?php endif; //if for if have slides ?>
+                    </div><!--.slides-->
+                </section><!--.homepage-slider-->
+                <section class="homepage-section-featured-project">
+                    <div class="process">
+                        <?php if(get_field("company_name","option")): ?>
+                            <p><?php the_field("company_name","option");?></p>
+                        <?php endif; ?>
+                        <?php if(get_field("process_description","option")): ?>
+                            <p><?php the_field("process description","option");?></p>
+                        <?php endif; ?>
+                        <?php if(get_field("process_link_text","option")): ?>
+                            <a href="<?php the_permalink(38); ?>">
+                                <?php the_field("process_link_text","option");?>
+                            </a>
+                        <?php endif; ?>
+                        <?php if(get_field("process_graphic","option")): ?>
+                            <img src="<?php wp_get_attachment_url(get_field("process_graphic"));?>" alt="process graphic">
+                        <?php endif; ?>
+                    </div><!--.process-->
+                    <div class="featured-video">
+                            <h2>Featured Video</h2>
+                        <div class="video-and-description">
+                            <div class="video">
+                                <?php the_field("featured_video","option");?>
+                            </div><!--.video-->
+                            <div class="description">
+                                <?php if(get_field("featured_video_title","option")&&get_field("featured_video_description","option")&&get_field("featured_video_link_text","option")): ?>
+                                    <h3><?php the_field("featured_video_title","option");?></h3>
+                                    <p><?php the_field("featured_video_description","option");?></p>
+                                    <a href="<?php the_permalink(82); ?>">
+                                        <?php the_field("featured_video_link_text","option");?>
+                                    </a>
+                                <?php endif; ?>
+                            </div><!--.description-->
+                            
+                        </div><!--.video-and-description
+                    </div><!-- .featured-video-->
+                </section><!--.homepage-featured-project-->
+                <section class="homepage-section-news">
+                    <h2>Latest News</h2>
+                    <?php $query=WP_Query(array('post_type'=>'post','posts_per_page'=>3));
+                    if($query->have_posts()):
+                        while($query->have_posts()): $query->the_post();?>
+                            <div class="news_tile">
+                                <?php $title=$query->post->post_title; ?>
+                                <?php if(has_post_thumbnail() && $title):?>
+                                    <img src="<?php wp_get_attachment_url();?>" alt="<?php echo $title; ?>" class="featured_image">
+                                <?php endif; //if for image ?>
+                                <p class="date"><?php the_date("n.j.Y")?></p>
+                                <h2 class="title"><?php echo $title ?></h2>
+                                <a href="<?php the_permalink(); ?>">MORE</a>
+                            </div><!--.news_tile-->
+                        <?php endwhile; //while for news posts
+                    endif; //if for news posts
+                    wp_reset_postdata();?>
+                </section><!--.homepage-news-->
+            <?php 
 			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
 		endif; ?>
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
